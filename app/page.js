0b7f0ac874b6f6d161b70810778a40ff542f0abe
Macmilan24 <cpt.macmilan24@gmail.com>
2024-08-02 14:05:39 +0300

    finished on uodating functionality

--- app/page.js
+++ app/page.js
@@ -24,7 +24,11 @@ import { firestore } from "@/firebase";
 export default function Home() {
   const [inventory, setInventory] = useState([]);
   const [open, setOpen] = useState(false);
+  const [Uopen, setUOpen] = useState(false);
   const [itemName, setItemName] = useState("");
+  const [selectedItem, setSelectedItem] = useState("");
+  const [updatedName, setUpdatedName] = useState("");
+  const [updatedQuantity, setUpdatedQuantity] = useState("");
 
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, "inventory"));
@@ -64,12 +68,25 @@ export default function Home() {
     updateInventory();
   };
 
+  const updateItems = async (item, updateData) => {
+    const docRef = doc(collection(firestore, "inventory"), item);
+    await setDoc(docRef, updateData, { merge: true });
+    updateInventory();
+  };
+
+  const handleUpdate = () => {
+    updateItems(selectedItem, { name: updatedName, quantity: updatedQuantity });
+    setUOpen(false);
+  };
+
   useEffect(() => {
     updateInventory();
   }, []);
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const handleUOpen = () => setUOpen(true);
+  const handleUClose = () => setUOpen(false);
   return (
     <Box
       width="100vw"
@@ -80,7 +97,7 @@ export default function Home() {
       alignItems="center"
       gap={2}
     >
-      <Modal open={open} onClose={handleClose}>
+      <Modal open={open} onClose={handleUClose}>
         <Box
           width="100vw"
           height="100vh"
@@ -121,6 +138,47 @@ export default function Home() {
           </Box>
         </Box>
       </Modal>
+
+      {/* update Modal */}
+      <Modal open={Uopen} onClose={handleClose}>
+        <Box
+          width="100vw"
+          height="100vh"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Box
+            width={400}
+            bgcolor="white"
+            border="2px solid #000"
+            boxShadow={24}
+            p={4}
+            display="flex"
+            flexDirection="column"
+            gap={3}
+          >
+            <Typography variant="h6">Update Item</Typography>
+            <TextField
+              variant="outlined"
+              label="Name"
+              value={updatedName}
+              onChange={(e) => setUpdatedName(e.target.value)}
+            />
+            <TextField
+              variant="outlined"
+              label="Quantity"
+              type="number"
+              value={updatedQuantity}
+              onChange={(e) => setUpdatedQuantity(e.target.value)}
+            />
+            <Button variant="outlined" onClick={handleUpdate}>
+              Update
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+
       <Button
         variant="contained"
         onClick={() => {
@@ -129,6 +187,7 @@ export default function Home() {
       >
         Add New Item
       </Button>
+
       <Box border="1px solid #333">
         <Box
           width="800px"
@@ -168,6 +227,15 @@ export default function Home() {
                 }}
               >
                 Remove item
+              </Button>
+              <Button
+                variant="contained"
+                onClick={() => {
+                  setSelectedItem(name);
+                  handleUOpen();
+                }}
+              >
+                Update Item
               </Button>
             </Box>
           ))}
